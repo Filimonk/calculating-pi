3ac033e20520fecf1fec3e867923e4c03599d302
Filimonk <61885735+Filimonk@users.noreply.github.com>
2024-02-20 15:55:28 +0300

    Update PI.cpp

--- PI.cpp
+++ PI.cpp
@@ -1,17 +1,48 @@
 #include<iostream>
 #include<vector>
 #include<math.h>
+#include<time.h>
 
-#include<LNum.h>
+#include "LNum.h"
 
 int main() {
     int accuracy;
+    
     std::cout << "Задайте точность" << std::endl;
     std::cin >> accuracy;
+    std::cout << std::endl;
+    
     LNum::setAccuracy(accuracy);
-    std::cout << "\n";
     
+    clock_t start = clock();
     
+    LNum PI;
+    LNum four = LNum(4);
+    for (int k = 1, i = 1; ; k += 2, ++i) {
+        LNum term;
+        
+        LNum divider = LNum(k);
+        term = four / divider;
+        
+        if (term == LNum(0)) {
+            break;
+        }
+        
+        if (k / 2 % 2) {
+            term = -term;
+        }
+        
+        PI = PI + term;
+        if (i % 1000 == 0) {
+            PI.print();
+        }
+    }
+    
+    clock_t finish = clock();
+    
+    PI.print();
+    std::cout << std::endl;
+    std::cout << static_cast<double>(finish - start) / CLOCKS_PER_SEC << "сек\n";
 
     return 0;
 }
